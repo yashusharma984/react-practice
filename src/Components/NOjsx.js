010c18dfd197c6ecaa90702d7d7b66cb8352930a
Yash Bhardwaj <132735857+yashusharma984@users.noreply.github.com>
2024-07-11 22:06:27 +0530

    commit -m

--- src/Components/NOjsx.js
+++ src/Components/NOjsx.js
@@ -1,6 +1,6 @@
 import React from "react";
 
 const NOjsx = ()=>{
-    return React.createElement('div', null ,<h1>Hello here no jsx used</h1>)
+    return React.createElement('div', null ,<h1>Hello here no jsx used here</h1>)
 }
 export default NOjsx;
