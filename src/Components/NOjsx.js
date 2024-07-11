903545141e5b323fd22df8253bc8781eb01a2150
Yash Bhardwaj <132735857+yashusharma984@users.noreply.github.com>
2024-07-11 22:32:44 +0530

    commit -m

--- src/Components/NOjsx.js
+++ src/Components/NOjsx.js
@@ -1,6 +1,16 @@
 import React from "react";
 
-const NOjsx = ()=>{
-    return React.createElement('div', null ,<h1>Hello here no jsx used here</h1>)
+// const NOjsx = ()=>{
+//     return React.createElement('div className= 'dummyClass', null ,<h1>Hello here no jsx used here</h1>)
+// }
+
+
+// with new method of no use of JSX....
+const NOjsx=()=>{
+return React.createElement(
+    'div',   // parameter 
+    {id: 'hello', className:'dummyClass'}, // it give the div has a id of  ... 3rd  use for value 
+    React.createElement('h1',null,'hello yash here ')
+)
 }
 export default NOjsx;
