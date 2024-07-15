24113d5835a5d5f678b4e8c5030e4b0e7fdf1c09
Yash Bhardwaj <132735857+yashusharma984@users.noreply.github.com>
2024-07-15 15:02:23 +0530

    commit -m

--- src/Binding Event Handler/EventBind.js
+++ src/Binding Event Handler/EventBind.js
@@ -8,20 +8,39 @@ import React, { Component } from 'react'
       this.state = {
          message: 'Hello'
       }
+    //   3rd method binding in the class constructor...
+    //   this.clickHandler = this.clickHandler.bind(this)
     }
     
-clickHandler(){
-    // this.setState({
-    //     message: 'Good Bye!'
-    // })
-    console.log(this)
+// clickHandler(){
+//     this.setState({
+//         message: 'Good Bye!'
+//     })
+//     console.log(this)
+// }
+
+//  4th method binding in class property of Arrow function
+clickHandler=()=>{
+    this.setState({
+        message : 'good Bye'
+    })
 }
+
 
   render() {
     return (
       <div>
         <div>{this.state.message}</div>
-        <button onClick={this.clickHandler}>Event Bound</button>
+
+        {/*1st method here...  binding  in render method */}
+        {/* <button onClick={this.clickHandler.bind(this)}>Event Bound</button> */}
+
+        {/* 2nd Method... arrow function in render method */}
+        {/* <button onClick={()=>this.clickHandler()} >Arrow Event</button> */}
+
+        {/* 3rd method .. bind in  class constructor  */}
+        <button onClick={this.clickHandler}> Bind </button>
+
       </div>
     )
   }
